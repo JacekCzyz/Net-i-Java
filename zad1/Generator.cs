ad160af59c0f66cd92599007a045cff4132df8fc
JacekCzyz <72863363+JacekCzyz@users.noreply.github.com>
2023-03-11 16:32:08 +0100

    Zad1

---
+++ zad1/Generator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace JacekiMarcin{
+    internal class Generator{
+        Random random;
+
+        public Generator(int seed){
+            this.random = new Random(seed);
+        }
+
+        public int rand(int low, int up){
+            return random.Next(low, up);
+        }
+        public override string ToString()
+        {
+            int x = 1;
+            string str = "tekst" + x.ToString() + " test";
+            return "to jest nasz generator";
+        }
+    }
+}
