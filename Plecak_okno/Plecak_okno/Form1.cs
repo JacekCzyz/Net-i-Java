38599c58b9bef0d5aa89edce4818a84d97b8d49e
Jacek Czyz <259265@student.pwr.edu.pl>
2023-03-23 22:15:13 +0100

    Dodaj pliki projektów.

---
+++ Plecak_okno/Plecak_okno/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Plecak_okno
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int seed = int.Parse(textBox1.Text);
+            int amount = int.Parse(textBox2.Text);
+            int limit = int.Parse(textBox3.Text);
+
+            textBox4.Text = seed.ToString();
+        }
+
+
+        
+
+
+    }
+}
---
+++ UnitTestProject1/UnitTestProject1/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("UnitTestProject1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("UnitTestProject1")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("66799c36-d092-4f34-8a3e-b7c3624ebcfb")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
