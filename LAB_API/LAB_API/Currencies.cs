8c103c88c6b8e971998452b4f19e9e8744e25e0c
Jacek Czyz <259265@student.pwr.edu.pl>
2023-04-13 22:23:31 +0200

    Finished api

---
+++ LAB_API/LAB_API/Currencies.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LAB_API
+{
+    public class Currencies
+    {
+        Dictionary<string, string> Mone;
+
+        public Dictionary<string, string> return_Mone()
+        { return Mone; }
+        public void Fill_Mone()
+        {
+            string data = this.Connect_symbol();
+            Mone = new Dictionary<string, string>();
+
+            Dictionary<string, object> nazwa = JsonConvert.DeserializeObject<Dictionary<string, object>>(data);
+
+            if ((bool)nazwa["success"])
+            {
+                var temp = (JObject)nazwa["symbols"];
+                Mone=temp.ToObject<Dictionary<string, string>>();
+            }
+        }
+
+
+        public double Convert(string from_currency, string to_currency, double amount)
+        {
+            string data2 = this.Connect_convert($"exchangerates_data/convert?to={to_currency}&from={from_currency}&amount={amount}");
+
+            Dictionary<string, object> response = JsonConvert.DeserializeObject<Dictionary<string, object>>(data2);
+            if ((bool)response["success"])
+                return (double)response["result"];
+            else
+                return -1;
+        }
+
+        public string Connect_symbol()
+        {
+            var client = new RestClient("https://api.apilayer.com");
+
+            var request = new RestRequest("exchangerates_data/symbols", Method.Get);
+            request.AddHeader("apikey", "3zyKCBy9DYDbFx8KdLckK1D8UMDRfaSo");
+
+            RestResponse response = client.Execute(request);
+            return response.Content;
+        }
+
+        public string Connect_convert(string url)
+        {
+            var client = new RestClient($"https://api.apilayer.com/");
+
+            var request = new RestRequest(url, Method.Get);
+            request.AddHeader("apikey", "3zyKCBy9DYDbFx8KdLckK1D8UMDRfaSo");
+
+            RestResponse response = client.Execute(request);
+            return response.Content;
+        }
+    }
+}
---
+++ LAB_API/LAB_API/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace LAB_API
+{
+    public partial class Form1 : Form
+    {
+        Currencies currency;
+        public Form1()
+        {
+            InitializeComponent();
+            currency = new Currencies();
+            currency.Fill_Mone();
+            fill_combobox();
+        }
+
+
+        private void fill_combobox()
+        {
+            Dictionary<string, string> text1 = currency.return_Mone();
+
+            comboBox1.DataSource = new BindingSource(text1, null);
+            comboBox1.DisplayMember = "value";
+            comboBox1.ValueMember = "key";
+
+
+
+            comboBox2.DataSource = new BindingSource(text1, null);
+            comboBox2.DisplayMember = "value";
+            comboBox2.ValueMember = "key";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("tdudsdsd");
+            string from_currency = ((KeyValuePair<string, string>)comboBox1.SelectedItem).Key;
+            string to_currency = ((KeyValuePair<string, string>)comboBox2.SelectedItem).Key;
+            double amount = double.Parse(textBox1.Text);
+            double converted=currency.Convert(from_currency, to_currency, amount);
+            textBox2.Text = converted.ToString();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selected_key = comboBox1.SelectedValue.ToString();
+            string selected_value = comboBox1.Text;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selected_key = comboBox2.SelectedValue.ToString();
+            string selected_value = comboBox2.Text;
+        }
+    }
+}
