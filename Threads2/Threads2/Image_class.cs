03ebcc197ce885c48154162f9375560c6b6ae672
Jacek Czyz <259265@student.pwr.edu.pl>
2023-04-27 16:08:28 +0200

    kubuś class

---
+++ Threads2/Threads2/Image_class.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threads2
+{
+    public class Image_class
+    {
+        public Bitmap image { get; set; }
+
+        public void LoadPgmImage(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                // PGM format header
+                var firstLine = reader.ReadLine();
+                if (!firstLine.StartsWith("P2"))
+                {
+                    throw new ArgumentException("Invalid PGM format.");
+                }
+
+                // Read image size
+                var sizeLine = reader.ReadLine().Split(' ');
+                var width = int.Parse(sizeLine[0]);
+                var height = int.Parse(sizeLine[2]);
+
+                // Read maximum color value
+                var maxColorValueLine = reader.ReadLine();
+                var maxColorValue = int.Parse(maxColorValueLine);
+
+                // Read image data
+                var bitmap = new Bitmap(width, height);
+                for (int y = 0; y < height; y++)
+                {
+                    var pixelValueLine = reader.ReadLine().Split(' ');
+                    int counter = 0;
+                    for (int x = 0; x < width; x++)
+                    {
+                        var pixelValue = int.Parse(pixelValueLine[counter]);
+                        var colorValue = (int)(pixelValue / (double)maxColorValue * 255);
+                        var color = Color.FromArgb(colorValue, colorValue, colorValue);
+                        bitmap.SetPixel(x, y, color);
+                        counter += 2;
+                    }
+                }
+                this.image = bitmap;                
+            }
+        }
+     }
+}
--- Threads2/Threads2/Program.cs
+++ Threads2/Threads2/Program.cs
@@ -18,7 +18,8 @@ namespace Threads2 {
             System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
 
-            Bitmap image = LoadPgmImage("C:\\Users\\jacek\\source\\repos\\Net\\Threads2\\Threads2\\kubus2.pgm");
+            Image_class Picture = new Image_class();
+            Picture.LoadPgmImage("C:\\Users\\jacek\\source\\repos\\Net\\Threads2\\Threads2\\kubus2.pgm");
 
 
             // Create a new form and picture box
@@ -26,55 +27,30 @@ namespace Threads2 {
             PictureBox pictureBox1 = new PictureBox();
 
             // Set the picture box size to match the image size
-            pictureBox1.Size = image.Size;
+            pictureBox1.Size = Picture.image.Size;
 
             // Set the image as the picture box content
-            pictureBox1.Image = image;
+            pictureBox1.Image = Picture.image;
 
             // Add the picture box to the form and show the form
             form.Controls.Add(pictureBox1);
             System.Windows.Forms.Application.Run(form);
+
+
+            // Create a new form and picture box
+            Form form2 = new Form();
+            PictureBox pictureBox2 = new PictureBox();
+
+            // Set the picture box size to match the image size
+            pictureBox2.Size = Picture.image.Size;
+
+            // Set the image as the picture box content
+            pictureBox2.Image = Picture.image;
+
+            // Add the picture box to the form and show the form
+            form.Controls.Add(pictureBox2);
+            System.Windows.Forms.Application.Run(form2);
         }
-
-        public static Bitmap LoadPgmImage(string filePath) {
-            using (var reader = new StreamReader(filePath)) {
-                // PGM format header
-                var firstLine = reader.ReadLine();
-                Console.WriteLine("First line: {0}", firstLine);
-                if (!firstLine.StartsWith("P2")) {
-                    throw new ArgumentException("Invalid PGM format.");
-                }
-
-                // Read image size
-                var sizeLine = reader.ReadLine().Split(' ');
-                var width = int.Parse(sizeLine[0]);
-                var height = int.Parse(sizeLine[2]);
-                Console.WriteLine("Width: {0}, Height: {1}", width, height);
-
-                // Read maximum color value
-                var maxColorValueLine = reader.ReadLine();
-                Console.WriteLine("Maximum color value line: {0}", maxColorValueLine);
-                var maxColorValue = int.Parse(maxColorValueLine);
-
-                // Read image data
-                var bitmap = new Bitmap(width, height);
-                for (int y = 0; y < height; y++) {
-                    var pixelValueLine = reader.ReadLine().Split(' ');
-                    int counter = 0;
-                    for (int x = 0; x < width; x++)
-                    {
-                        var pixelValue = int.Parse(pixelValueLine[counter]);
-                        var colorValue = (int)(pixelValue / (double)maxColorValue * 255);
-                        var color = Color.FromArgb(colorValue, colorValue, colorValue);
-                        bitmap.SetPixel(x, y, color);
-                        counter += 2;
-                    }
-                }
-
-                return bitmap;
-            }
-        }
-
     }
 }
 
